2320eddc3273ac84e985b802bf18801d8726353e
inamhussain5911 <inamh1021@gmail.com>
2024-11-08 19:27:47 +0500

    inital commit

---
+++ script.js
@@ -0,0 +1,62 @@
+// const MyArr = [0,1,2,3,4,5]
+// console.log(arr[0])
+
+// arr.push(5911)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+
+// const newarr = arr.join()
+// console.log(arr)
+// console.log(newarr)
+
+// console.log("a ", MyArr)
+
+// const myn1 = MyArr.slice(1,3)
+// console.log(myn1)
+// console.log("b ", MyArr)
+
+
+
+
+// const myn2 = MyArr.splice(1,3)
+// console.log("c ", MyArr)
+// console.log(myn2)
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// console.log(Math.random())
+// // console.log(Math.ceil(4.1))
+// // console.log(Math.round(4.4))
+// // console.log(Math.floor(4.8))
+// // console.log(Math.max(22,3,44,55,66))
+// // console.log(Math.min(33,44,77,15,33))
+
+
+// const min = 10
+// const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min +1))+min);
+
+
+
+// +++++++++++++++++++   array  +++++++++++++++++++++
+
+const MyArr = [0,1,2,3,4,5]
+// MyArr.push(myone)
+// console.log(MyArr)
+
+
+
+console.log("a ",  MyArr)
+const daily = MyArr.slice(1,3)
+console.log(daily)
+console.log("b ", MyArr)
+const dail = MyArr.splice(1,3)
+console.log(MyArr)
+console.log("c ", dail);
